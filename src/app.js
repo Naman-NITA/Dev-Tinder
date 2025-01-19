f3d969734cd2ee032a5e44891b86646bd5f69209
Naman-NITA <namank89123@gmail.com>
2025-01-20 00:52:15 +0530

    Authenication

--- src/app.js
+++ src/app.js
@@ -1,29 +1,86 @@
 
 const express = require("express");
 
- const connectDB = require("./config/database"); 
+const connectDB = require("./config/database"); 
 
 const app = express();
 
 const User = require("./models/user");
 
+const bcrypt = require("bcrypt");
+
 app.use(express.json());
 
- // signup the user to the database
- app.post("/signup" , async ( req, res) => {
+
+const {validateSignupData} = require("./utils/validation");
+const { isStrongPassword } = require("validator");
+
+ 
+
+app.post("/signup", async (req, res) => {
+
+  try {
+
+  // validation of data is required
+  validateSignupData(req);
+
+  // Encrypt the password
+ 
+
+  const { firstName , lastName , emailId , password } = req.body;
+
+  const passwordHash = await bcrypt.hash(password,10);
+
+  console.log(passwordHash);
+
 
   
-   console.log(req.body);
 
-  //Creating a new instance of the User model
-       const user = new User(req.body);
-    try {
-      await user.save();  
-      res.send("User Added Successfully 2");
-    } catch (error) {
-      res.status(400).send("Error saving the user:" + err.message);
+     
+ // creating a new instance of the user model
+  const user = new User({
+    firstName,
+    lastName,
+    emailId,
+    password: passwordHash,
+  });
+ 
+    await user.save();
+    res.status(201).send("User added successfully");
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).send("Email ID already exists");
+    } else {
+      res.status(400).send("ERROR : " + error.message);
     }
- });
+  }
+});
+
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const isPasswordsValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordsValid) {
+      res.send("Login Successful !!!");
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+});
+
+
+
 
   //get user by email
   app.get("/user" , async(req,res) => {
@@ -47,7 +104,7 @@ app.use(express.json());
 
   });
 
-  // get all the data from the database
+ 
   app.get("/feed" ,async (req,res) => {
     
      try { 
@@ -62,7 +119,9 @@ app.use(express.json());
   });
 
 
-  //Deleted the data from the database
+
+
+
 
   app.delete("/delete" , async(req,res) =>{
      
@@ -82,37 +141,61 @@ app.use(express.json());
   });
 
 
-  // Updqate the data from the database
 
-   app.patch("/user" , async (req,res) => {
-    const userId = req.body.userId;
 
+  app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId; 
     const data = req.body;
+  
     try {
-      await User.findByIdAndUpdate({_id : userId } , data);
+      const ALLOWED_UPDATE = ["gender", "photoUrl", "age", "about", "skills"];
+  
+    
+      const isUpdateAllowed = Object.keys(data).every((key) => ALLOWED_UPDATE.includes(key)); 
 
-      res.send("User Updated successfully");
+      if(!isUpdateAllowed){
+       throw new Error("Update not allowed");
+      }
 
-      
+      if(data?.skills.length > 10){
+        throw new Error("Skills cannot be more than 10"); 
+      }
+     
+    const user = await User.findByIdAndUpdate({_id : userId} , data , {
+          returnDocument : "after",
+          runValidators : true,
+    });
+  
+      // // Check if the user exists
+      // if (!updatedUser) {
+      //   return res.status(404).send("User not found");
+      // }
+  
+      // Return the updated user
+      // console.log(user);
+      res.send("User updated successfully"); 
+  
     } catch (error) {
-      res.status(400).send("Something went wrong");
+      res.status(400).send("Something went wrong: " + error.message);
     }
-   });
-
+  });
+  
+  
 
 
 connectDB()
  .then(() => {
   console.log("Database connection established....");
 
-  app.listen(3000 , () => {
-    console.log("Server is running in port 3000...");
+  app.listen(4000 , () => {
+    console.log("Server is running in port 4000...");
    });
 
  })
  .catch((err) => {
   console.log("Database cannot be connected!!");
  });
+
 
 
 

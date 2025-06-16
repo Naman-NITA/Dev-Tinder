1e5e60f7bb3ddd4d2bca0b958d42523c406a2541
Naman-NITA <namank89123@gmail.com>
2025-06-16 22:17:46 +0530

    Fix import  Vercel

--- src/app.js
+++ src/app.js
@@ -15,9 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://tinder-frontent-9hpt.vercel.app"
+  ],
   credentials: true,
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());

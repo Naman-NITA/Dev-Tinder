481d35b1d071d89acea81c5f310c6b90930039ee
Naman-NITA <namank89123@gmail.com>
2025-07-06 16:23:00 +0530

    Fix import issues for Vercel

--- src/models/user.js
+++ src/models/user.js
@@ -1,82 +1,85 @@
 
- const mongoose = require('mongoose');
- 
-  const validator  = require("validator");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-  const bcrypt = require("bcrypt");
-
-  const jwt = require("jsonwebtoken");
-
- const userSchema = new mongoose.Schema({
-
-  firstName: {
-   type: String,
-   required : true,
-   minLength:4,
-   maxLength : 50,
-  },
-
-  lastName: {
-    type: String
-  },
- 
-  emailId: {
-    type: String,
-    lowercase: true,
-    required: true,
-    unique: true,
-    trim: true,
-    validate: {
-      validator: (value) => validator.isEmail(value), 
-      message: (props) => `Invalid email address: ${props.value}`, 
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password: " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
+    },
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+    membershipType: {
+      type: String,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [String],
     },
   },
-  
-
-
-  password : {
-    type: String,
-    required : true,
-    validate: {
-      validator: (value) => validator.isStrongPassword(value), 
-      message: (props) => `Enter a strong passwords: ${props.value}`, 
-    },
-  },
-
-  age: {
-    type : Number,   
-    min: 18,
-    
-  },
-
-  gender: {
-    type: String,
-    validate(value){
-     if(!["male","female","others"].includes(value)){
-      throw new Error("Gender data is not valid");
-     }
-    },
-  },
-
-  photoUrl:{
-    type : String,
-    validate: {
-      validator: (value) => validator.isURL(value), 
-      message: (props) => `Invalid Photo url: ${props.value}`, 
-    },
-  },
- about : {
-  type: String,
-  default : "This is a default about of the user!",
- },
- skills: {
-  type: [String],
- },
-
- },{
-  timestamps:true,
- });
-
+  {
+    timestamps: true,
+  }
+);
 
   userSchema.index({firstName : 1 , lastName :1});
 
